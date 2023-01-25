9dd2be1ba5f1e214867f4ab4276fb00c9530a6f9
Pedrog01 <pedro7109.j@gmail.com>
2023-01-25 10:31:28 -0300

    Creating the controller for Questions

---
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -0,0 +1,40 @@
+class AdminsBackoffice::QuestionsController <
+    AdminsBackofficeController
+     before_action :set_question, only: [:edit, :update, :destroy]
+    
+    def index
+         @questions = Question.all.order(:description).page(params[:page])
+    end
+    
+     def new
+        @question = Question.new
+     end
+
+     def update
+            if @question.update(params_question)
+            redirect_to admins_backoffice_question_path, notice: "Questão
+        atualizada com sucesso!"
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+            if @question.destroy
+            redirect_to admins_backoffice_question_path, notice: "Questão
+            excluída com sucesso!"
+        else
+            render :index
+        end
+    end
+
+        private
+
+        def params_question
+            params.require(:question).permit(:description, :subject_id)
+        end
+
+        def set_question
+            @question = Question.find(params[:id])
+        end
+       end
