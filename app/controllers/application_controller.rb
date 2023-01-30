5a0f575dcae135563d8f422677d909ba769d6d1e
Pedrog01 <pedro7109.j@gmail.com>
2023-01-30 08:32:19 -0300

    Configuring a template for the Admin Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,5 @@
 class ApplicationController < ActionController::Base
 
-    #layout :layout_by_resource
-    #private
-    #def layout_by_resource
-    #puts ">>>>>>>>>>>>>>>>> #{devise_controller?}"
-    #puts ">>>>>>>>>>>>>>>>> #{resource_class}"
-    #"application"
-    #end
-
     layout :layout_by_resource
 
   protected
