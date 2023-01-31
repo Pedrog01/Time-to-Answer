ee3d1f9688f2ab156ea86390abf3d61dc0399136
Pedrog01 <pedro7109.j@gmail.com>
2023-01-31 08:26:17 -0300

    Using class methods in the model

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,6 +1,6 @@
 class Site::SearchController <  SiteController
     def questions
     #    @questions = Question.includes(:answers).page(params[:page])
-    @questions = Question.includes(:answers).where("lower(description) LIKE ? ","%#{ params[:term].downcase}%")
+    @questions = Question.search(params[:page], params[:term])
     end
-end
+end 
