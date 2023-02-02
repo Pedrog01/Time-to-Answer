0ac2b4aa3f2fe11ed5656bbe209211d82696fa0a
Pedrog01 <pedro7109.j@gmail.com>
2023-02-02 12:47:09 -0300

    Refactoring the UserStatistic

--- app/models/user_statistic.rb
+++ app/models/user_statistic.rb
@@ -6,4 +6,15 @@ class UserStatistic < ApplicationRecord
   def total_questions
     self.right_questions + self.wrong_questions
   end
+
+  #class Methods
+  private
+
+    def self.set_user_statistic(answers,current_user)
+    if !!current_user 
+        user_statistic = UserStatistic.find_or_create_by(user: current_user)
+    answer.correct?  ? user_statistic.right_questions += 1 : user_statistic.wrong_questions += 1  
+      user_statistic.save
+    end
+  end
 end
