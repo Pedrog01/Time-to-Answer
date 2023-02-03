df331b35624c453a9b37862bcab759f239ace395
Pedrog01 <pedro7109.j@gmail.com>
2023-02-03 09:39:55 -0300

    Refactoring AdminStatistic

--- app/models/admin_statistic.rb
+++ app/models/admin_statistic.rb
@@ -4,8 +4,18 @@ class AdminStatistic < ApplicationRecord
         total_questions: "TOTAL_QUESTIONS"
         }
 
+    #Scopes
 
-        #class Method
+    scope :total_users, ->{
+        find_by_event(EVENTS[:total_users]).value
+    }
+
+    scope :total_questions, ->{
+        find_by_event(EVENTS[:total_questions])
+    }
+
+
+    #class Method
     def self.set_event(event)
         admin_statistic = AdminStatistic.find_or_create_by(event: event)
         admin_statistic.value += 1
